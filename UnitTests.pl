2f04e548f3590d1e9761aa548b08909d44346c01
ChristianMassiveCross <chrisbea@gmx.net>
2015-02-28 19:14:08 +0100

    initial commit for tests

---
+++ UnitTests.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+package UnitTests;
+use strict;
+print(".: Call Mockify Unit tests :.\n");
+my $TestDirectory = 't';
+my @UnitTestsModules = glob("$TestDirectory/*.t");
+
+foreach my $UnitTestModul (@UnitTestsModules){
+	system('perl', ($UnitTestModul));
+}
+
+1;
