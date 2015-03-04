28de7749b6885fc459f559db8e146f50b4b14efe
ChristianMassiveCross <chrisbea@gmx.net>
2015-03-04 21:50:10 +0100

    half done is valid perl magic is missing

--- Tools.pm
+++ Tools.pm
@@ -18,5 +18,28 @@ sub loadPackage {
     load($PackageFileName);
     return;
 }
+#------------------------------------------------------------------------
+sub isValid {
+	my $self = shift;
+    my ($Value) = @_;
 
+    my $IsValid = 0;
+    if( defined($Value) && $Value ne '' ){
+        $IsValid = 1;
+        #exclude empty hash
+        if( ref($Value) eq 'HASH' ){
+            if( scalar keys %{$Value} == 0 ){
+                $IsValid = 0;
+            }
+            # do compare with { '' => undef } ???
+            use Data::Dumper;
+            print Dumper values $Value;
+        } 
+        #exclude empty array
+        if( ref($Value) eq 'ARRAY' && (scalar @{$Value}) == 0){
+            $IsValid = 0;
+        }
+    }
+	return $IsValid;
+}
 1;
