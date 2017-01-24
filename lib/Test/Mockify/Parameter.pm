51c09ec4394580e5e6b96a95f847efb559bf1b51
Christian Breitkreutz <cbreitkreutz@epages.com>
2017-01-24 19:50:36 +0100

    finish return value

--- lib/Test/Mockify/Parameter.pm
+++ lib/Test/Mockify/Parameter.pm
@@ -9,45 +9,45 @@ sub new {
     my $class = shift;
     my ($aExpectedParams) = @_;
     my $self  = bless {
-    	'ExpectedParams' => $aExpectedParams,
+        'ExpectedParams' => $aExpectedParams,
     }, $class;
     return $self;
 }
 #---------------------------------------------------------------------
 sub call {
-	my $self = shift;
-	return $self->{'ReturnValue'}->call();
+    my $self = shift;
+    return $self->{'ReturnValue'}->call();
 }
 #---------------------------------------------------------------------
 sub buildReturn {
-	my $self = shift;
-	$self->{'ReturnValue'} = Test::Mockify::ReturnValue->new();
-	return $self->{'ReturnValue'};	
+    my $self = shift;
+    $self->{'ReturnValue'} = Test::Mockify::ReturnValue->new();
+    return $self->{'ReturnValue'};    
 }
 #---------------------------------------------------------------------
 sub compareExpectedParameters {
-	my $self = shift;
-	my ($Parameters) = @_;
-	return unless (scalar @{$Parameters} == scalar @{$self->{'ExpectedParams'}});
-	return Data::Compare->new()->Cmp($Parameters, $self->{'ExpectedParams'});
+    my $self = shift;
+    my ($Parameters) = @_;
+    return unless (scalar @{$Parameters} == scalar @{$self->{'ExpectedParams'}});
+    return Data::Compare->new()->Cmp($Parameters, $self->{'ExpectedParams'});
 }
 #---------------------------------------------------------------------
 sub matchWithExpectedParameters {
-	my $self = shift;
-	my @Params = @_;
-	return unless (scalar @Params == scalar @{$self->{'ExpectedParams'}});
-	for(my $i=0; $i < scalar @Params; $i++){		
-		if(IsString($self->{'ExpectedParams'}->[$i]) && $self->{'ExpectedParams'}->[$i] eq 'NoExpectedParameter'){
-		    next;			
-		}elsif(ref($Params[$i]) eq $self->{'ExpectedParams'}->[$i]){# map classname
-			next;
-		}elsif(Data::Compare->new()->Cmp($Params[$i], $self->{'ExpectedParams'}->[$i])){
-		    next;
-		} else{
-			return 0;
-		}
-	}
-	return 1;
+    my $self = shift;
+    my @Params = @_;
+    return unless (scalar @Params == scalar @{$self->{'ExpectedParams'}});
+    for(my $i=0; $i < scalar @Params; $i++){        
+        if(IsString($self->{'ExpectedParams'}->[$i]) && $self->{'ExpectedParams'}->[$i] eq 'NoExpectedParameter'){
+            next;            
+        }elsif(ref($Params[$i]) eq $self->{'ExpectedParams'}->[$i]){# map classname
+            next;
+        }elsif(Data::Compare->new()->Cmp($Params[$i], $self->{'ExpectedParams'}->[$i])){
+            next;
+        } else{
+            return 0;
+        }
+    }
+    return 1;
 }
 
 1;
--- lib/Test/Mockify/ReturnValue.pm
+++ lib/Test/Mockify/ReturnValue.pm
@@ -1,31 +1,72 @@
 package Test::Mockify::ReturnValue;
+use strict;
+use warnings;
+#------------------------------------------------------------
 sub new {
     my $class = shift;
     my $self  = bless {
     }, $class;
     return $self;
 }
+#------------------------------------------------------------
 sub thenReturn {
     my $self = shift;
     my ($Value) = @_;
-    $self->{'value'} = $Value;
+    $self->{'Value'} = $Value;
 }
+#------------------------------------------------------------
+sub thenReturnArray {
+    my $self = shift;
+    my ($Value) = @_;
+    die('NoAnArrayRef') unless(ref($Value) eq 'ARRAY');
+    $self->{'ArrayValue'} = $Value;
+}
+#------------------------------------------------------------
+sub thenReturnHash {
+    my $self = shift;
+    my ($Value) = @_;
+    die('NoAnHashRef') unless(ref($Value) eq 'HASH');
+    $self->{'HashValue'} = $Value;
+}
+#------------------------------------------------------------
 sub thenReturnUndef {
     my $self = shift;
-    $self->{'value'} = undef;
+    $self->{'UndefValue'} = 1;
 }
+#------------------------------------------------------------
 sub thenThrowError {
     my $self = shift;
-    my ($ErrorType) = @_;
-    $self->{'ErrorType'} = $ErrorType;
+    my ($ErrorCode) = @_;
+    die('NoErrorCode') unless($ErrorCode);
+    $self->{'ErrorType'} = $ErrorCode;
     return;
 }
+#------------------------------------------------------------
+sub thenCall{
+    my $self = shift;
+    my ($FunctionPointer) = @_;
+    die('NoAnCodeRef') unless(ref($FunctionPointer) eq 'CODE');
+    $self->{'FunctionPointer'} = $FunctionPointer;
+    return;
+}
+#------------------------------------------------------------
 sub call {
     my $self = shift;
+    my @Params = @_;
     if($self->{'ErrorType'}){
         die($self->{'ErrorType'});
+    }elsif($self->{'ArrayValue'}){
+        return @{$self->{'ArrayValue'}};
+    }elsif($self->{'HashValue'}){
+        return %{$self->{'HashValue'}};
+    }elsif($self->{'UndefValue'}){
+        return;
+    }elsif($self->{'FunctionPointer'}){
+        return $self->{'FunctionPointer'}->(@Params);
+    }elsif($self->{'Value'}){
+        return $self->{'Value'};
     }else{
-        return $self->{'value'};
+        die('NoReturnValue');
     }
 }
 1;
